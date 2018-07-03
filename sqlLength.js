9da76ccd8245f4824332d62736d6ee43e0754628
Ty Atkins <36547246+atkinsta@users.noreply.github.com>
2018-07-03 12:58:38 -0400

    Updated sqlLength to allow command-line arguments
    Allows the user to run this as a node CLI. 
    Takes two arguments - filename and number of columns. 
    Example - node sqlLength.js TopAlbums.csv 8

--- sqlLength.js
+++ sqlLength.js
@@ -1,8 +1,9 @@
 let LineByLineReader = require("line-by-line"), // npm install line-by-line for this to work.
-    lr = new LineByLineReader("TopSongs.csv");  // put your file in here, example ("data.csv").
+    lr = new LineByLineReader(process.argv[2]);  // put your file in here, example ("data.csv").
 
-let maxLength = Array(9).fill(0);               // Put the number of columns you need to check in here like Array(num).
-let columnLocator = Array(9).fill("");          // Ditto
+let numCols = parseInt(process.argv[3])
+let maxLength = Array(numCols).fill(0);               // Put the number of columns you need to check in here like Array(num).
+let columnLocator = Array(numCols).fill("");          // Ditto
 
 lr.on("error", function(err){
     throw err;
@@ -14,21 +15,15 @@ lr.on("line", function(line) {
         if (word.length > maxLength[index]) {
             maxLength[index] = word.length;
             columnLocator[index] = word;
-            console.log("Updated MaxLength[%i] to max length %i", index, word.length);
+            // console.log("Updated MaxLength[%i] to max length %i", index, word.length);
         }
     });
 });
 
 lr.on("end", function() {
-    console.log("\nLongest Column 1: %s - Length: %s\n" +
-                "Longest Column 2: %s - Length: %s\n" +
-                "Longest Column 3: %s - Length: %s\n" +
-                "Longest Column 4: %s - Length: %s\n" +
-                "Longest Column 5: %s - Length: %s\n",
-                columnLocator[0].toString(), maxLength[0].toString(),
-                columnLocator[1].toString(), maxLength[1].toString(),
-                columnLocator[2].toString(), maxLength[2].toString(),
-                columnLocator[3].toString(), maxLength[3].toString(),
-                columnLocator[4].toString(), maxLength[4].toString()); //Convert all to strings to allow multiple data types.
+    columnLocator.forEach((column, index) => {
+        console.log("Column%i Max Length: %i - Longest Entry: %s",
+                    index, maxLength[index], column.toString());
+    })
     console.log("I'm done here");
 });
